cdbc5e3abf9b19ac24f4b8c826748daa97c4c183
criskwy <119081205+criskwy@users.noreply.github.com>
2025-03-10 12:08:51 -0600

    commit final styles js

--- main.js
+++ main.js
@@ -3,18 +3,32 @@ let submitBtn = document.querySelector(".submit-btn");
 let ratingContainer = document.querySelector(".rating-container");
 let thankyouStateContainer = document.querySelector(".thankyou-state-container");
 let thankyouScore = document.querySelector(".thankyou-score")
+let selectedScore = null;
 
+/** score button state */
 
-scoreContainer.addEventListener("click", event=> {
-    if(event.target.innerHTML > 0 || event.target.innerHTML <= 5){
+scoreContainer.addEventListener("click", event => {
+    if (event.target.classList.contains("score-num")) {
+        let score = parseInt(event.target.innerHTML);
 
-        thankyouScore.innerHTML = event.target.innerHTML;
+        if (score > 0 && score <= 5) {
 
-
-        submitBtn.addEventListener("click", ()=>{
-
-            thankyouStateContainer.style.display="flex";
-            ratingContainer.style.display = "none";
-        })
+            selectedScore = score;
+            thankyouScore.innerHTML = score;
+        }
     }
+
+
+})
+
+/** submit button state */
+
+submitBtn.addEventListener("click", () => {
+    if (selectedScore !== null) {
+        thankyouStateContainer.style.display = "flex";
+        ratingContainer.style.display = "none";
+    } else {
+        alert("Please, select a score!")
+    }
+
 })
